99c7d3405ea8fbb8485583b317db0ac6f9f1049b
Dario Turin <dturin1997@gmail.com>
2023-02-13 11:57:29 -0500

    Video-08-udemy: Basic Styling

--- Main.jsx
+++ Main.jsx
@@ -1,8 +1,7 @@
-import { View, Text, SafeAreaView, StatusBar, Platform } from 'react-native'
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import Home from "./screens/Home"
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from "./screens/Home";
 
 const Stack = createNativeStackNavigator()
 
@@ -15,7 +14,7 @@ const Main = () => {
         </Stack.Group>
     </Stack.Navigator>
   </NavigationContainer>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;

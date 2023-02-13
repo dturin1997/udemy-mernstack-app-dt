2192558cdb812e2eacf1a4dd4a8be78077fd891f
Dario Turin <dturin1997@gmail.com>
2023-02-13 13:13:42 -0500

    Video-12-udemy: Categories Component

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,19 +1,36 @@
-import { View, Text, TouchableOpacity } from "react-native";
-import React from "react";
+import { View, Text, TouchableOpacity, ScrollView } from "react-native";
+import React, { useState } from "react";
 import { colors, defaultStyle } from "../styles/styles";
 import Header from "../components/Header";
-import { Avatar } from "react-native-paper";
+import { Avatar, Button } from "react-native-paper";
 
 const Home = () => {
+  const categories = [
+    { category: "Nice", _id: "sdjlajla" },
+    { category: "Nice2", _id: "sdjlajla2" },
+    { category: "Nice3", _id: "sdjlajla3" },
+    { category: "Nice4", _id: "sdjlajla4" },
+    { category: "Nice5", _id: "sdjlajla5" },
+    { category: "Nice6", _id: "sdjlajla6" },
+  ];
+
+  const [ category, setCategory ] = useState("");
+
+  const categoryButtonHadler = (id) => {
+    setCategory(id);
+  };
+  console.log(category);
+
   return (
     <View style={defaultStyle}>
       <Header />
+      {/* Heading Row */}
       <View
         style={{
           paddingTop: 70,
           flexDirection: "row",
           justifyContent: "space-between",
-          alignItems: "center"  
+          alignItems: "center",
         }}
       >
         {/* Heading */}
@@ -38,6 +55,43 @@ const Home = () => {
           </TouchableOpacity>
         </View>
       </View>
+      {/* Categories */}
+      <View
+        style={{
+          flexDirection: "row",
+          height: 80,
+        }}
+      >
+        <ScrollView
+          contentContainerStyle={{
+            alignItems: "center"
+          }} 
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        >
+          {categories.map((item, index) => (
+            <Button
+              key={item._id}
+              style={{
+                backgroundColor: category === item._id ? colors.color1 : colors.color5,
+                borderRadius: 100,
+                margin: 5,
+              }}
+              onPress={() => categoryButtonHadler(item._id)}
+            >
+              <Text
+                style={{
+                  fontSize: 12,
+                  color: category === item._id ? colors.color2 : "gray",
+                }}
+              >
+                {item.category}
+              </Text>
+            </Button>
+          ))}
+        </ScrollView>
+      </View>
+      {/* Products */}
     </View>
   );
 };

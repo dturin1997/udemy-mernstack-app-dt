88670fc3d04b723cfff738d79e14d69b3caec325
Dario Turin <dturin1997@gmail.com>
2023-02-13 22:57:46 -0500

    Video-17-udemy: Designing Product Details Layout

--- screens/ProductDetails.jsx
+++ screens/ProductDetails.jsx
@@ -1,8 +1,16 @@
-import { View, Text, Dimensions, Image, StyleSheet } from "react-native";
-import React, { useRef } from "react";
+import {
+  View,
+  Text,
+  Dimensions,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import React, { useRef, useState } from "react";
 import { colors, defaultStyle } from "../styles/styles";
 import Header from "../components/Header";
 import Carousel from "react-native-snap-carousel";
+import { Avatar } from "react-native-paper";
 
 const SLIDER_WIDTH = Dimensions.get("window").width;
 const ITEM_WIDTH = SLIDER_WIDTH;
@@ -18,7 +26,22 @@ export const iconOptions = {
 };
 
 const ProductDetails = ({ route: { params } }) => {
+  const name = "Macbook pro";
+  const price = 4999;
+  const description =
+    "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas , las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum";
+  const stock = 5;
   const isCarousel = useRef(null);
+  const [quantity, setQuantity] = useState(1);
+
+  const incrementQty = () => {
+    if (stock <= quantity) return;
+    setQuantity((prev) => prev + 1);
+  };
+  const decrementQty = () => {
+    if (quantity <= 1) return;
+    setQuantity((prev) => prev - 1);
+  };
 
   const images = [
     {
@@ -50,6 +73,82 @@ const ProductDetails = ({ route: { params } }) => {
         data={images}
         renderItem={CarouselCardItem}
       />
+      <View
+        style={{
+          backgroundColor: colors.color2,
+          padding: 35,
+          flex: 1,
+          marginTop: -380,
+          borderTopLeftRadius: 55,
+          borderTopRightRadius: 55,
+        }}
+      >
+        <Text
+          numberOfLines={2}
+          style={{
+            fontSize: 25,
+          }}
+        >
+          {name}
+        </Text>
+
+        <Text
+          style={{
+            fontSize: 18,
+            fontWeight: "900",
+          }}
+        >
+          ${price}
+        </Text>
+
+        <Text
+          style={{
+            letterSpacing: 1,
+            lineHeight: 20,
+            marginVertical: 15,
+          }}
+          numberOfLines={8}
+        >
+          {description}
+        </Text>
+
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            paddingHorizontal: 5,
+          }}
+        >
+          <Text
+            style={{
+              color: colors.color3,
+              fontWeight: "100",
+            }}
+          >
+            Quantity
+          </Text>
+
+          <View
+            style={{
+              width: 80,
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <TouchableOpacity onPress={decrementQty}>
+              <Avatar.Icon icon={"minus"} {...iconOptions} />
+            </TouchableOpacity>
+
+            <Text style={style.quantity}>{quantity}</Text>
+
+            <TouchableOpacity onPress={incrementQty}>
+              <Avatar.Icon icon={"plus"} {...iconOptions} />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };

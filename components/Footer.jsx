90678383df02bc2549d058d604deeb31b83c429a
Dario Turin <dturin1997@gmail.com>
2023-04-16 22:30:38 -0500

    Video-30-udemy: Building Profile Screen

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = ({ activeRoute = "home" }) => {
   const navigate = useNavigation();
 
   const loading = false;
-  const isAuthenticated = false;
+  const isAuthenticated = true;
 
   const navigationHandler = (key) => {
     switch (key) {
@@ -70,7 +70,13 @@ const Footer = ({ activeRoute = "home" }) => {
           >
             <Avatar.Icon
               {...avatarOptions}
-              icon={activeRoute === "profile" ? "account" : "account-outline"}
+              icon={
+                isAuthenticated === false
+                  ? "login"
+                  : activeRoute === "profile"
+                  ? "account"
+                  : "account-outline"
+              }
             />
           </TouchableOpacity>
         </View>

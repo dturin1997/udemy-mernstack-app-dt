324c6ea5491f6f99d29913fbd8988045a77a03da
Dario Turin <dturin1997@gmail.com>
2023-04-24 23:25:39 -0500

    Video-31-udemy: Button Box Component

--- screens/Profile.jsx
+++ screens/Profile.jsx
@@ -2,60 +2,111 @@ import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import React, { useState } from "react";
 import { colors, defaultStyle, formHeading } from "../styles/styles";
 import { Avatar, Button } from "react-native-paper";
+import ButtonBox from "../components/ButtonBox";
+import Footer from "../components/Footer";
 
 const user = {
   name: "Dario",
   email: "sample@gmail.com",
 };
 
+const loading = false;
+
 const Profile = ({ navigation }) => {
   const [avatar, setAvatar] = useState(null);
+
+  const navigateHandler = () => {};
+
   return (
-    <View style={{ ...defaultStyle, paddingBottom: 80 }}>
-      {/* Heading */}
-      <View
-        style={{
-          marginBottom: 20,
-        }}
-      >
-        <Text style={formHeading}>Profile</Text>
-      </View>
-      {/* Loading */}
-      <View style={styles.container}>
-        <Avatar.Image
-          source={{
-            uri: avatar,
-          }}
-          sourcesize={100}
-          style={{ backgroundColor: colors.color1 }}
-        />
-        <TouchableOpacity
-          onPress={() => navigation.navigate("camera", { updateProfile: true })}
-        >
-          <Button textColor={colors.color1}>Change Photo</Button>
-        </TouchableOpacity>
-
-        <Text style={styles.name}>{user?.name}</Text>
-        <Text
-          style={{
-            fontWeight: "300",
-            color: colors.color2,
-          }}
-        >
-          {user?.email}
-        </Text>
-      </View>
-
-      <View>
+    <>
+      <View style={{ ...defaultStyle, paddingBottom: 80 }}>
+        {/* Heading */}
         <View
           style={{
-            flexDirection: "row",
-            margin: 10,
-            justifyContent: "space-between",
+            marginBottom: 20,
           }}
-        ></View>
+        >
+          <Text style={formHeading}>Profile</Text>
+        </View>
+        {/* Loading */}
+        <View style={styles.container}>
+          <Avatar.Image
+            source={{
+              uri: avatar,
+            }}
+            sourcesize={100}
+            style={{ backgroundColor: colors.color1 }}
+          />
+          <TouchableOpacity
+            onPress={() =>
+              navigation.navigate("camera", { updateProfile: true })
+            }
+          >
+            <Button textColor={colors.color1}>Change Photo</Button>
+          </TouchableOpacity>
+
+          <Text style={styles.name}>{user?.name}</Text>
+          <Text
+            style={{
+              fontWeight: "300",
+              color: colors.color2,
+            }}
+          >
+            {user?.email}
+          </Text>
+        </View>
+
+        <View>
+          <View
+            style={{
+              flexDirection: "row",
+              margin: 10,
+              justifyContent: "space-between",
+            }}
+          >
+            <ButtonBox
+              handler={navigateHandler}
+              text={"Orders"}
+              icon={"format-list-bulleted-square"}
+            />
+            {user?.role === "admin" && (
+              <ButtonBox
+                handler={navigateHandler}
+                icon={"view-dashboard"}
+                text={"Admin"}
+                reverse={true}
+              />
+            )}
+            <ButtonBox
+              handler={navigateHandler}
+              text={"Profile"}
+              icon={"pencil"}
+            />
+          </View>
+
+          <View
+            style={{
+              flexDirection: "row",
+              margin: 10,
+              justifyContent: "space-evenly",
+            }}
+          >
+            <ButtonBox
+              handler={navigateHandler}
+              text={"Password"}
+              icon={"pencil"}
+            />
+            <ButtonBox
+              handler={navigateHandler}
+              text={"Sign Out"}
+              icon={"exit-to-app"}
+            />
+          </View>
+        </View>
       </View>
-    </View>
+
+      <Footer />
+    </>
   );
 };
 
